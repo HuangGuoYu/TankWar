0d15659f4ac7a3d1787ee5c67a7ce74c0facdc0d
guoyu <huangguoyu1106@163.com>
2017-10-24 21:56:05 +0800

    可移动的点

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,6 +1,54 @@
-public class Client {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Client extends Frame{
+
+    int x = 50,y = 50;
+    /**
+     * 启动 start the frame
+     */
+    public void launch() {
+        this.setLocation(300, 100);
+        this.setSize(800, 600);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
+        this.setBackground(Color.CYAN);
+        this.setVisible(true);
+    }
+
+    /**
+     * 重画，复写的父类方法，自动调用
+     * @param g
+     */
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(c);
+
+        y += 5;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Client client = new Client();
+        client.launch();
+    }
+
+    /**
+     * 重画线程
+     */
+    private class RepaintThread implements Runnable{
+
+        public void run() {
+            while(true) {
+                repaint();
+            }
+        }
     }
 }
