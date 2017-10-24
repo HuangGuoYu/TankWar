606c9ae388518dd783ed2a2cd890ee1d83d4db6e
guoyu <huangguoyu1106@163.com>
2017-10-24 23:05:37 +0800

    第六节实现了控制移动

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,16 +1,21 @@
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Client extends Frame{
 
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
+    public static final double PACE = 5;
     int x = 50,y = 50;
     /**
      * 启动 start the frame
      */
     public void launch() {
         this.setLocation(300, 100);
-        this.setSize(800, 600);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -19,20 +24,20 @@ public class Client extends Frame{
         });
         this.setResizable(false);
         this.setBackground(Color.CYAN);
+        this.addKeyListener(new KeyMonitor());
         this.setVisible(true);
         //移动线程
         new Thread(()->{
             while(true) {
                 repaint();
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
     }
-
     /**
      * 重画，复写的父类方法，自动调用
      * @param g
@@ -42,15 +47,12 @@ public class Client extends Frame{
         g.setColor(Color.RED);
         g.fillOval(x, y, 30, 30);
         g.setColor(c);
-
-        y += 5;
     }
 
     public static void main(String[] args) {
         Client client = new Client();
         client.launch();
     }
-
     /**
      * 重画线程
      */
@@ -65,6 +67,28 @@ public class Client extends Frame{
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    private class KeyMonitor extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode(); //获得当前的键代码
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    x -= PACE;
+                    break;
+                case KeyEvent.VK_UP:
+                    y -= PACE;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    x += PACE;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    y += PACE;
+                    break;
+            }
+
         }
     }
 }
